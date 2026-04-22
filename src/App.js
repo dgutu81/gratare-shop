90fcfe51ded7e8b896663f4c9cff393a1d2f0198
dgutu81 <dgutu81@gmail.com>
2026-04-22 10:34:49 +0300

    fix unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 
 // ── INITIAL PRODUCTS DATA ──────────────────────────────────────────────────
 const INITIAL_PRODUCTS = [
